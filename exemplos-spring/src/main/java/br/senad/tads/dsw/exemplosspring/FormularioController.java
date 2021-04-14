ceb240ba674c458e05e3a92b81808e9f523de4c4
ftsuda-senac <ftsuda.senac@gmail.com>
2021-04-13 21:09:30 -0300

    Exemplo POST com POST-REDIRECT-GET

--- exemplos-spring/src/main/java/br/senad/tads/dsw/exemplosspring/FormularioController.java
+++ exemplos-spring/src/main/java/br/senad/tads/dsw/exemplosspring/FormularioController.java
@@ -5,12 +5,16 @@
  */
 package br.senad.tads.dsw.exemplosspring;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/formulario")
@@ -19,6 +23,29 @@ public class FormularioController {
     @GetMapping
     public ModelAndView mostrarForm() {
         ModelAndView mv = new ModelAndView("formulario");
+        mv.addObject("dados", new DadosPessoais());
+        return mv;
+    }
+
+    @GetMapping("/preenchido")
+    public ModelAndView mostrarFormPreenchido() {
+        DadosPessoais dados = new DadosPessoais();
+        dados.setId(1);
+        dados.setNome("Seu Madruga");
+        dados.setEmail("madruga@teste.com.br");
+        dados.setTelefone("(11) 99999-1234");
+        dados.setSenha("abcd1234");
+        dados.setRepetirSenha("abcd1234");
+        dados.setDescricao("A vingança nunca é plena");
+        dados.setNumero(70);
+        dados.setPeso(new BigDecimal("70.5"));
+        dados.setAltura(new BigDecimal("1.81"));
+        dados.setDataNascimento(LocalDate.parse("1970-01-20"));
+        dados.setGenero(1);
+        dados.setInteresses(Arrays.asList("Esportes", "Viagens"));
+
+        ModelAndView mv = new ModelAndView("formulario");
+        mv.addObject("dados", dados);
         return mv;
     }
     
@@ -28,6 +55,20 @@ public class FormularioController {
         ModelAndView mv = new ModelAndView("resultado");
         mv.addObject("dados", dados);
         return mv;
+    }
+
+    @PostMapping("/salvar-post-redirect-get")
+    public ModelAndView salvarDadosPostRedirectGet(
+            @ModelAttribute DadosPessoais dados,
+            RedirectAttributes redirAttr) {
+        ModelAndView mv = new ModelAndView("redirect:/formulario/resultado-post-redirect-get");
+        redirAttr.addFlashAttribute("dados", dados);
+        return mv;
+    }
+
+    @GetMapping("/resultado-post-redirect-get")
+    public ModelAndView mostrarResultado() {
+        return new ModelAndView("resultado");
     }
     
 }
