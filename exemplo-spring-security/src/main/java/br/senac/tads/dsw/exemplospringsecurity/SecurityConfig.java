3a025c8c223ddd2636aa0d5e5309cdad4929eccb
ftsuda-senac <ftsuda.senac@gmail.com>
2021-05-18 21:54:07 -0300

    Exemplo Spring Security

--- exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/SecurityConfig.java
+++ exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/SecurityConfig.java
@@ -4,24 +4,75 @@
  */
 package br.senac.tads.dsw.exemplospringsecurity;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 /**
  *
  * @author fernando.tsuda
  */
-// @Configuration
-// @EnableWebSecurity
-public class SecurityConfig /* extends WebSecurityConfigurerAdapter */ {
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true,
+    securedEnabled = true,
+    jsr250Enabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /*
-     * public static PasswordEncoder plainPasswordEncoder() { return new PasswordEncoder() {
-     * 
-     * @Override public String encode(CharSequence cs) { return cs.toString(); }
-     * 
-     * @Override public boolean matches(CharSequence cs, String hashSenha) { return hashSenha !=
-     * null && hashSenha.equals(cs.toString()); } }; }
-     * 
-     * public static PasswordEncoder bcryptPasswordEncoder() { return new BCryptPasswordEncoder(); }
-     * 
-     * @Bean public PasswordEncoder passwordEncoder() { return plainPasswordEncoder(); }
-     */
+    public static PasswordEncoder plainPasswordEncoder() {
+        return new PasswordEncoder() {
+
+            @Override
+            public String encode(CharSequence cs) {
+                return cs.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence cs, String hashSenha) {
+                return hashSenha
+                        != null && hashSenha.equals(cs.toString());
+            }
+        };
+    }
+
+//    public static PasswordEncoder bcryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers()
+                .cacheControl().disable() // Desabilitar cache
+                .frameOptions().sameOrigin() // Habilitar cabeçalho x-frame-options para resolver problema do console H2
+            .and()
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/css/**", "/img/**", "/js/**", "/font/**", "/", "/index.html", "/h2/**").permitAll()
+//                    .antMatchers("/protegido/peao/**").hasRole("PEAO") // .hasAuthority("ROLE_PEAO")
+//                    .antMatchers("/protegido/fodon/**").hasRole("FODON")
+//                    .antMatchers("/protegido/god/**").hasRole("GOD")
+                    .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .usernameParameter("username")
+                    .passwordParameter("senha")
+                    .defaultSuccessUrl("/home").permitAll()
+            .and()
+                .exceptionHandling()
+                    .accessDeniedPage("/erro/403");
+    }
+
 }
